5c49ea7a0ef1e9e446ae1d5208fb6b422413c044
roese025 <nateroese@Nathans-MacBook-Pro-3.local>
2017-05-02 22:14:08 -0500

    short1

---
+++ js/script.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+	
+    $(".news").mouseenter(function() {
+		$("#line").animate({ left: '50px' });
+        $("#upline").animate({ left: '50px' });
+	});
+
+	$(".news").mouseleave(function() {
+		$("#line").animate({ left: '135px' });
+        $("#upline").animate({ left: '135px' });
+	});
+   
+    
+    
+    $(".sports").mouseenter(function() {
+		$("#line").animate({ left: '135px' });
+        $("#upline").animate({ left: '135px' });
+	});
+
+	$(".sports").mouseleave(function() {
+		$("#line").animate({ left: '135px' });
+        $("#upline").animate({ left: '135px' });
+	});
+    
+    
+    
+    $(".life").mouseenter(function() {
+		$("#line").animate({ left: '222px' });
+        $("#upline").animate({ left: '222px' });
+	});
+
+	$(".life").mouseleave(function() {
+		$("#line").animate({ left: '135px' });
+        $("#upline").animate({ left: '135px' });
+	});
+    
+    
+    
+    
+     $(".obituaries").mouseenter(function() {
+		$("#line").animate({ left: '322px' });
+        $("#upline").animate({ left: '322px' });
+	});
+
+	$(".obituaries").mouseleave(function() {
+		$("#line").animate({ left: '135px' });
+        $("#upline").animate({ left: '135px' });
+	});
+    
+    
+    
+    $(".opinion").mouseenter(function() {
+		$("#line").animate({ left: '450px' });
+        $("#upline").animate({ left: '450px' });
+	});
+
+	$(".opinion").mouseleave(function() {
+		$("#line").animate({ left: '135px' });
+        $("#upline").animate({ left: '135px' });
+	});
+    
+    
+    
+    
+    
+});
